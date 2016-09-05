188a2ba224ebe27990fb9edb198cddaeaad82705
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-09-05 14:50:05 +0200

    experimenting with own promise implementation

---
+++ promises.js
@@ -0,0 +1,124 @@
+function isCallable(a) {
+    return typeof a === "function";
+}
+
+function fulfil(promise, value) {
+    if (promise.state === "pending") {
+        promise.state = "fulfilled";
+        promise.value = value;
+        var reactions = promise.fulfilReactions;
+        promise.rejectReactions = [];
+        promise.fulfilReactions = [];
+        reactions.forEach(function (reaction) {
+            setTimeout(function () {
+                reaction(value);
+            }, 0);
+        });
+    }
+}
+
+function reject(promise, value) {
+    if (promise.state === "pending") {
+        promise.state = "rejected";
+        promise.value = value;
+        var reactions = promise.rejectReactions;
+        promise.rejectReactions = [];
+        promise.fulfilReactions = [];
+        reactions.forEach(function (reaction) {
+            setTimeout(function () {
+                reaction(value);
+            }, 0);
+        });
+    }
+}
+
+function Promise(executor) {
+    if (!isCallable(executor)) {
+        throw new TypeError("executor must be a function.");
+    }
+
+    this.value = null;
+    this.state = "pending";
+    this.fulfilReactions = [];
+    this.rejectReactions = [];
+
+    try {
+        executor(
+            function (x) {
+                fulfil(this, x);
+            }.bind(this),
+            function (x) {
+                reject(this, x);
+            }.bind(this)
+        );
+    } catch (e) {
+        reject(this, e);
+    }
+}
+
+Promise.prototype.then = function (onFulfilled, onFailure) {
+    var originalPromise = this;
+
+    return new Promise(function executor(resolve, reject) {
+
+        if (!isCallable(onFulfilled)) {
+            onFulfilled = function (x) {
+                return x;
+            }
+        }
+
+        if (!isCallable(onFailure)) {
+            onFailure = function (x) {
+                return reject(x);
+            };
+        }
+
+        if (originalPromise.state === "pending") {
+            originalPromise.fulfilReactions.push(function (value) {
+                try {
+                    resolve(onFulfilled(value));
+                } catch (e) {
+                    reject(e);
+                }
+            });
+            originalPromise.rejectReactions.push(function (value) {
+                try {
+                    resolve(onFailure(value));
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        } else if (originalPromise.state === "fulfilled") {
+            setTimeout(function () {
+                try {
+                    resolve(onFulfilled(originalPromise.value));
+                } catch (e) {
+                    reject(e);
+                }
+            }, 0);
+        } else if (originalPromise.state === "rejected") {
+            setTimeout(function () {
+                try {
+                    resolve(onFailure(originalPromise.value));
+                } catch (e) {
+                    reject(e);
+                }
+            }, 0);
+        }
+    });
+};
+
+Promise.resolve = function (x) {
+    return new Promise(function (resolve, reject) {
+        resolve(x);
+    });
+};
+
+Promise.reject = function (x) {
+    return new Promise(function (Resolve, reject) {
+        reject(x);
+    });
+};
+
+
+module.exports = Promise;
---
+++ test/my-adapter.js
@@ -0,0 +1,14 @@
+var Promise = require('../promises');
+
+module.exports = {
+    resolved: Promise.resolve,
+    rejected: Promise.reject,
+    deferred: function () {
+        var resolve, reject;
+        var promise = new Promise((f, g) => {
+            resolve = f;
+            reject = g
+        });
+        return {promise, resolve, reject}
+    }
+}
